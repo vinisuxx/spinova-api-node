7c808c78bc512a79ee42f8aa32ebe66352763d28
Vinícius Silva <vinisuxx@outlook.com>
2024-01-27 16:19:57 -0300

    🛠️ | additional configs

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,7 @@
+import { PrismaClient } from '@prisma/client';
 import { fastify } from 'fastify';
 
 export const app = fastify();
+
+export const prisma = new PrismaClient();
+
--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,10 @@
-import { app } from './app'
+import { app } from './app';
 import chalk from 'chalk';
+import { env } from './env';
 
 app.listen({
   host: '0.0.0.0',
-  port: 3333
+  port: env.PORT
 }).then(() => {
-  console.log(chalk.greenBright(`🆙 Server running!`));
-})
+  console.log(chalk.greenBright('🆙 Server running!'));
+});
